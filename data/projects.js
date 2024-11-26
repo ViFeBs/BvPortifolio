ef3e0df8c4f01d2f7e0a97d8cd56df32e0aca49c
ViFeBs <brunov25@outlook.com>
2024-11-26 07:14:07 -0300

    Meu portifolio com o tema de persona

---
+++ data/projects.js
@@ -0,0 +1,52 @@
+export const projects = [
+    {
+        id: 1,
+        name: "Projeto 1",
+        nameEn: "Project 1",
+        image: "./src/project-placeholder.jpg",
+        description: "Descrição breve do projeto 1.",
+        descriptionEn: "Brief description of Project 1.",
+        languages: ["JavaScript", "HTML", "CSS"],
+        link: "./projeto1.html"
+    },
+    {
+        id: 2,
+        name: "Projeto 2",
+        nameEn: "Project 2",
+        image: "./src/project-placeholder.jpg",
+        description: "Descrição breve do projeto 2.",
+        descriptionEn: "Brief description of Project 2.",
+        languages: ["Python", "Flask"],
+        link: "./projeto2.html"
+    },
+    {
+        id: 3,
+        name: "Projeto 3",
+        nameEn: "Project 3",
+        image: "./src/project-placeholder.jpg",
+        description: "Descrição breve do projeto 3.",
+        descriptionEn: "Brief description of Project 3.",
+        languages: ["C#", ".NET"],
+        link: "./projeto3.html"
+    },
+    {
+        id: 4,
+        name: "Projeto 4",
+        nameEn: "Project 4",
+        image: "./src/project-placeholder.jpg",
+        description: "Descrição breve do projeto 4.",
+        descriptionEn: "Brief description of Project 4.",
+        languages: ["React", "Node.js"],
+        link: "./projeto4.html"
+    },
+    {
+        id: 5,
+        name: "Projeto 5",
+        nameEn: "Project 5",
+        image: "./src/project-placeholder.jpg",
+        description: "Descrição breve do projeto 5.",
+        descriptionEn: "Brief description of Project 5.",
+        languages: ["PHP", "Laravel"],
+        link: "./projeto5.html"
+    }
+];
